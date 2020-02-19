def60acc2fbf58b03050d1bfb51b99daa5fe459c
zhouyuhe <yuhe.zhou@weareadaptive.com>
2020-02-19 09:45:30 +0000

    add remote

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import './index.css'
+import Table from './Table'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello, React!</h1>
+      <div className="container">
+        <Table />
       </div>
     )
   }
 }
 
-export default App;
+export default App
---
+++ react-tutorial/src/Table.js
@@ -0,0 +1,32 @@
+const TableBody = () => {
+    return <tbody />
+  }
+
+  class App extends Component {
+    render() {
+      const characters = [
+        {
+          name: 'Charlie',
+          job: 'Janitor',
+        },
+        {
+          name: 'Mac',
+          job: 'Bouncer',
+        },
+        {
+          name: 'Dee',
+          job: 'Aspring actress',
+        },
+        {
+          name: 'Dennis',
+          job: 'Bartender',
+        },
+      ]
+  
+      return (
+        <div className="container">
+          <Table />
+        </div>
+      )
+    }
+  }
